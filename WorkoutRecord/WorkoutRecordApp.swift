699d6f6aa231117a6bfb85ef6f6a2e7094b62810
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-22 21:52:07 -0400

    Initial Commit

---
+++ WorkoutRecord/WorkoutRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutRecordApp.swift
+//  WorkoutRecord
+//
+//  Created by Kiho Okazawa on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
